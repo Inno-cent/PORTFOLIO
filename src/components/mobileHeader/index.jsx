deb066d97b0ae987e115e88eadbe7129a10eab96
inno-cent <ekweozorinnocent@gmail.com>
2025-07-16 23:59:51 +0100

    feat: service

--- src/components/mobileHeader/index.jsx
+++ src/components/mobileHeader/index.jsx
@@ -77,7 +77,7 @@ const MobileHeader = () => {
             <p>Software Dev</p>
           </div>
         </Link>
-        <div
+        {/* <div */}
           className={`mobile__hamburger ${open ? "open" : ""}`}
           onClick={handleOpen}
         >
